1759f513c34e51d944edd1e96ea776543a298b37
Electr0 <114804446+Electr0Gunner@users.noreply.github.com>
2026-04-14 11:57:11 +0300

    added some more things to the debugging window

--- src/Lawn/Debug/DebuggerWindow.cpp
+++ src/Lawn/Debug/DebuggerWindow.cpp
@@ -2,7 +2,13 @@
 
 #include "DebuggerWindow.h"
 #include "../Board.h"
+#include "../ZenGarden.h"
 #include "../Widget/ChallengeScreen.h"
+#include "../Sexy.TodLib/EffectSystem.h"
+#include "../Sexy.TodLib/Attachment.h"
+#include "../Sexy.TodLib/Trail.h"
+#include "../Sexy.TodLib/Reanimator.h"
+#include "../Sexy.TodLib/TodParticle.h"
 #include "../Challenge.h"
 #include "../../LawnApp.h"
 #include "../../Sexy.TodLib/TodStringFile.h"
@@ -32,10 +38,99 @@ void DebuggerWindow::Update()
 			}
 			else
 			{
-				ImGui::Text("Currently on Board");
-				ImGui::SeparatorText("Challenge");
-				ImGui::Text("Current Challenge: %s", TodStringTranslate(mApp->GetCurrentChallengeDef().mChallengeName).c_str());
+				if (ImGui::BeginTabBar("BoardTabs"))
+				{
+					if (mApp->mGameMode == GameMode::GAMEMODE_CHALLENGE_ZEN_GARDEN)
+					{
+						if (ImGui::BeginTabItem("Zen Garden"))
+						{
+							ImGui::Text("Total Potted Plant Count: %d", mApp->mPlayerInfo->mNumPottedPlants);
+							for (int aGarden = 0; aGarden < GardenType::GARDEN_TOTAL; aGarden++)
+							{
+								if (aGarden == GARDEN_WHEELBARROW)
+									ImGui::SeparatorText("Wheelbarrow");
+								else
+									ImGui::SeparatorText(StrFormat("Garden %d", aGarden).c_str());
+								int aGardenPlantCount = 0;
+								for (int i = 0; i < mApp->mPlayerInfo->mNumPottedPlants; i++)
+								{
+									PottedPlant *aPottedPlant = mApp->mZenGarden->PottedPlantFromIndex(i);
+									if (aPottedPlant->mWhichZenGarden == aGarden)
+									{
+										aGardenPlantCount++;
+									}
+								}
+								ImGui::Text("Potten Plant Count: %d", aGardenPlantCount);
 
+							}
+
+							ImGui::EndTabItem();
+						}
+					}
+					else if (mApp->mGameMode == GameMode::GAMEMODE_TREE_OF_WISDOM)
+					{
+						ImGui::Text("Tree Height: %d", mApp->mPlayerInfo->mChallengeRecords[mApp->GetCurrentChallengeIndex()]);
+
+					}
+					else
+					{
+						if (ImGui::BeginTabItem("Challenge"))
+						{
+							ImGui::Text("Current Challenge: %s", TodStringTranslate(mApp->GetCurrentChallengeDef().mChallengeName).c_str());
+							ImGui::SeparatorText("Waves");
+
+							ImGui::Text("Wave Count: %d", mApp->mBoard->mNumWaves);
+							ImGui::Text("Flag Count: %d", mApp->mBoard->mNumWaves / mApp->mBoard->GetNumWavesPerFlag());
+
+							ImGui::Text("Current Wave: %d", mApp->mBoard->mCurrentWave);
+							ImGui::Text("Current Flag: %d", mApp->mBoard->mCurrentWave / mApp->mBoard->GetNumWavesPerFlag());
+
+
+							ImGui::EndTabItem();
+						}
+						if (ImGui::BeginTabItem("Zombies"))
+						{
+							ImGui::SeparatorText("Zombie Spawning");
+							for (int i = 0; i < 100; i++)
+							{
+								if (mApp->mBoard->mZombieAllowed[i])
+								{
+									ImGui::Text(GetZombieDefinition((ZombieType)i).mZombieName);
+								}
+							}
+
+							ImGui::EndTabItem();
+						}
+						if (ImGui::BeginTabItem("Stats"))
+						{
+							ImGui::SeparatorText("Game Objects");
+							ImGui::Text("Plants: %d", mApp->mBoard->mPlants.mSize);
+							ImGui::Text("Zombies: %d", mApp->mBoard->mZombies.mSize);
+							ImGui::Text("Projectiles: %d", mApp->mBoard->mProjectiles.mSize);
+							ImGui::Text("Coins: %d", mApp->mBoard->mCoins.mSize);
+							ImGui::Text("Lawn Mowers: %d", mApp->mBoard->mLawnMowers.mSize);
+							ImGui::Text("Grid Items: %d", mApp->mBoard->mGridItems.mSize);
+
+							ImGui::SeparatorText("Generic Objects");
+							ImGui::Text("Reanimations: %d", mApp->mEffectSystem->mReanimationHolder->mReanimations.mSize);
+
+							ImGui::Text("Particle Systems: %d", mApp->mEffectSystem->mParticleHolder->mParticleSystems.mSize);
+							ImGui::Text("Particles: %d", mApp->mEffectSystem->mParticleHolder->mParticles.mSize);
+							ImGui::Text("Emitters: %d", mApp->mEffectSystem->mParticleHolder->mEmitters.mSize);
+
+							ImGui::Text("Trails: %d", mApp->mEffectSystem->mTrailHolder->mTrails.mSize);
+
+							ImGui::Text("Attachments: %d", mApp->mEffectSystem->mAttachmentHolder->mAttachments.mSize);
+
+
+							ImGui::EndTabItem();
+						}
+
+					}
+
+
+					ImGui::EndTabBar();
+				}
 			}
 				
 			ImGui::EndTabItem();
